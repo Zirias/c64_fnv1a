488f2a59b3e0af4796a71bd35f147667b13f7a76
Felix Palmen <felix@palmen-it.de>
2026-01-20 09:35:12 +0100

    fnv1a_fast: Cleanup and micro-optimize
    Eliminate a "magic number", change a branch so that the common case is
    "not taken" (saving cycles), and eliminate unnecessary TAY instruction.

--- fnv1a_fast.s
+++ fnv1a_fast.s
@@ -5,6 +5,7 @@
 
 fnv1a_initval:	.byte	$25, $23, $22, $84, $e4, $9c, $f2, $cb
 fnv1a_steps:	.byte	$01, $02, $01, $03, $01
+fnv1a_nsteps=	* - fnv1a_steps
 
 .zeropage
 
@@ -13,18 +14,19 @@ fnv1a_tmp:	.res	8
 
 .code
 
+fnv1a_done:	rts
+
 fnv1a:
-		sta	fnv1a_rd+1
-		stx	fnv1a_rd+2
+		sta	fnv1a_mainloop+1
+		stx	fnv1a_mainloop+2
 		ldx	#7
 fnv1a_initloop:	lda	fnv1a_initval,x
 		sta	fnv1a_hash,x
 		dex
 		bpl	fnv1a_initloop
-fnv1a_rd:	lda	$ffff
-		bne	fnv1a_mainloop
-		rts
-fnv1a_mainloop:	eor	fnv1a_hash
+fnv1a_mainloop:	lda	$ffff
+		beq	fnv1a_done
+		eor	fnv1a_hash
 		sta	fnv1a_hash
 		sta	fnv1a_tmp
 		lda	fnv1a_hash+1
@@ -49,9 +51,8 @@ fnv1a_mainloop:	eor	fnv1a_hash
 		adc	fnv1a_tmp+2
 		sta	fnv1a_hash+7
 
-		ldx	#4
-fnv1a_mult:	lda	fnv1a_steps,x
-		tay
+		ldx	#fnv1a_nsteps-1
+fnv1a_mult:	ldy	fnv1a_steps,x
 
 fnv1a_shift:	asl	fnv1a_tmp
 		rol	fnv1a_tmp+1
@@ -64,7 +65,7 @@ fnv1a_shift:	asl	fnv1a_tmp
 		dey
 		bne	fnv1a_shift
 
-fnv1a_add:	clc
+		clc
 		lda	fnv1a_tmp
 		adc	fnv1a_hash
 		sta	fnv1a_hash
@@ -92,8 +93,8 @@ fnv1a_add:	clc
 		dex
 		bpl	fnv1a_mult
 
-fnv1a_next:	inc	fnv1a_rd+1
-		bne	fnv1a_rdok
-		inc	fnv1a_rd+2
-fnv1a_rdok:	jmp	fnv1a_rd
+		inc	fnv1a_mainloop+1
+		bne	fnv1a_next
+		inc	fnv1a_mainloop+2
+fnv1a_next:	jmp	fnv1a_mainloop
 
