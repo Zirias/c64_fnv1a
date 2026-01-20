5751f13a886a24bc194518f8e4562c616de42f4c
Felix Palmen <felix@palmen-it.de>
2026-01-20 10:03:44 +0100

    fnv1a: Reformat and comment
    Use loop expansions to avoid repetitive source. Add comments to document
    how it works.

--- fnv1a.s
+++ fnv1a.s
@@ -2,99 +2,87 @@
 .exportzp fnv1a_hash
 
 .data
-
+		; Initial hash value: 0xcbf29ce484222325
 fnv1a_initval:	.byte	$25, $23, $22, $84, $e4, $9c, $f2, $cb
+FNV1A_SIZE=	* - fnv1a_initval
+
+		; 64bit FNV-1a prime is 0x00000100000001b3
+		; Table of the number of necessary left shifts to multiply by
+		; just 0x1b3, in backwards order for simple x-indexed reading:
 fnv1a_steps:	.byte	$01, $02, $01, $03, $01
-fnv1a_nsteps=	* - fnv1a_steps
+FNV1A_NSTEPS=	* - fnv1a_steps
+		; The table excludes the single bit position needed to multiply
+		; by 0x0000010000000000, because it's an outlier (which would
+		; require *excessive* bit shifting) AND happens to have a
+		; position that's a multiple of 8, so it can be special-cased
+		; without executing any actual bit shifts for our optimized
+		; implementation.
 
 .zeropage
 
-fnv1a_hash:	.res	8
-fnv1a_tmp:	.res	8
+fnv1a_hash:	.res	FNV1A_SIZE	; Buffer for the 64bit hash value
+fnv1a_tmp:	.res	FNV1A_SIZE	; Temporary buffer used for shift/add
 
 .code
 
-fnv1a_done:	rts
+fnv1a_done:	rts	; routine exit, branches here when done hashing
 
+; fnv1a: Calculate 64bit FNV-1A hash for a given NUL-terminated input.
+;
+;	A/X (in)		pointer to NUL-terminated input
+;	fnv1a_hash (ZP,out)	64bit hash in little endian
+;
+;	clobbers:		A, X, Y, SR (all flags)
+;
 fnv1a:
-		sta	fnv1a_mainloop+1
-		stx	fnv1a_mainloop+2
-		ldx	#7
-fnv1a_initloop:	lda	fnv1a_initval,x
+		sta	fnv1a_mainloop+1	; initialize pointer to
+		stx	fnv1a_mainloop+2	; input
+		ldx	#FNV1A_SIZE-1
+fnv1a_initloop:	lda	fnv1a_initval,x		; initialize hash value
 		sta	fnv1a_hash,x
 		dex
 		bpl	fnv1a_initloop
-fnv1a_mainloop:	lda	$ffff
-		beq	fnv1a_done
-		eor	fnv1a_hash
-		sta	fnv1a_hash
-		sta	fnv1a_tmp
-		lda	fnv1a_hash+1
-		sta	fnv1a_tmp+1
-		lda	fnv1a_hash+2
-		sta	fnv1a_tmp+2
-		lda	fnv1a_hash+3
-		sta	fnv1a_tmp+3
-		lda	fnv1a_hash+4
-		sta	fnv1a_tmp+4
-		lda	fnv1a_hash+5
-		sta	fnv1a_tmp+5
-		clc
-		adc	fnv1a_tmp
-		sta	fnv1a_hash+5
-		lda	fnv1a_hash+6
-		sta	fnv1a_tmp+6
-		adc	fnv1a_tmp+1
-		sta	fnv1a_hash+6
-		lda	fnv1a_hash+7
-		sta	fnv1a_tmp+7
-		adc	fnv1a_tmp+2
-		sta	fnv1a_hash+7
 
-		ldx	#fnv1a_nsteps-1
-fnv1a_mult:	ldy	fnv1a_steps,x
+fnv1a_mainloop:	lda	$ffff			; read next input byte
+		beq	fnv1a_done		; stop on NUL
+		eor	fnv1a_hash		; xor with current hash
+		sta	fnv1a_hash		; write lowest byte to
+		sta	fnv1a_tmp		; both buffers
+		.repeat	5, B
+		lda	fnv1a_hash+1+B		; copy the next 5 bytes
+		sta	fnv1a_tmp+1+B		; to temp buffer
+		.endrep
+		clc				; special case multiplication
+		adc	fnv1a_tmp		; by 0x0000010000000000 ->
+		sta	fnv1a_hash+5		; add value shifted by 5 bytes
+		.repeat 2, B
+		lda	fnv1a_hash+6+B		; copy and add remaining
+		sta	fnv1a_tmp+6+B		; two bytes
+		adc	fnv1a_tmp+1+B
+		sta	fnv1a_hash+6+B
+		.endrep
 
-fnv1a_shift:	asl	fnv1a_tmp
-		rol	fnv1a_tmp+1
-		rol	fnv1a_tmp+2
-		rol	fnv1a_tmp+3
-		rol	fnv1a_tmp+4
-		rol	fnv1a_tmp+5
-		rol	fnv1a_tmp+6
-		rol	fnv1a_tmp+7
+		ldx	#FNV1A_NSTEPS-1
+fnv1a_mult:	ldy	fnv1a_steps,x		; multiply by 0x1b3 using table
+
+fnv1a_shift:	asl	fnv1a_tmp		; shift temp buffer left
+		.repeat FNV1A_SIZE-1, B
+		rol	fnv1a_tmp+1+B
+		.endrep
 		dey
-		bne	fnv1a_shift
+		bne	fnv1a_shift		; repeat as needed
 
-		clc
-		lda	fnv1a_tmp
-		adc	fnv1a_hash
-		sta	fnv1a_hash
-		lda	fnv1a_tmp+1
-		adc	fnv1a_hash+1
-		sta	fnv1a_hash+1
-		lda	fnv1a_tmp+2
-		adc	fnv1a_hash+2
-		sta	fnv1a_hash+2
-		lda	fnv1a_tmp+3
-		adc	fnv1a_hash+3
-		sta	fnv1a_hash+3
-		lda	fnv1a_tmp+4
-		adc	fnv1a_hash+4
-		sta	fnv1a_hash+4
-		lda	fnv1a_tmp+5
-		adc	fnv1a_hash+5
-		sta	fnv1a_hash+5
-		lda	fnv1a_tmp+6
-		adc	fnv1a_hash+6
-		sta	fnv1a_hash+6
-		lda	fnv1a_tmp+7
-		adc	fnv1a_hash+7
-		sta	fnv1a_hash+7
+		clc				; add temp buffer to hash value
+		.repeat FNV1A_SIZE, B
+		lda	fnv1a_tmp+B
+		adc	fnv1a_hash+B
+		sta	fnv1a_hash+B
+		.endrep
 		dex
-		bpl	fnv1a_mult
+		bpl	fnv1a_mult		; repeat until end of table
 
-		inc	fnv1a_mainloop+1
-		bne	fnv1a_next
+		inc	fnv1a_mainloop+1	; update input pointer to
+		bne	fnv1a_next		; next input byte
 		inc	fnv1a_mainloop+2
-fnv1a_next:	jmp	fnv1a_mainloop
+fnv1a_next:	jmp	fnv1a_mainloop		; and repeat
 
